c957aafd7f179d10e9b4d10795276c3f810c602b
JasinskiKacper <kacperjasinski78@gmail.com>
2026-01-15 21:39:02 +0100

    add terminal menu for mode selection

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import modes.classic
+import os
+mode = ['Classic', 'Quote', 'Ability', 'Emoji', 'Splash']
 
-print('=== Modes ===')
-print('1. Classic\n2. Quote\n3. Ability\n4. Emoji\n5. Splash')
+def clear_terminal():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-mode = str(input('Enter Mode: '))
+
+
+if __name__ == '__main__':
+
+    print(f'=== Modes ===\n1.{mode[0]}\n2.{mode[1]}\n3.{mode[2]}\n4.{mode[3]}\n5.{mode[4]}\n0.Exit')
+
+    choice = int(input('\nEnter mode: '))
+
+    clear_terminal()
+
+    if choice == 0:
+        print('See you next time!')
+        break
